fb28ba628edfdc0d059c0f29f9f28b958f47b8a2
jordan15396 <jordanlillis12@outlook.com>
2022-11-11 19:11:03 +0000

    added locators file and removed xpaths from test file

---
+++ Locators/locators.py
@@ -0,0 +1,16 @@
+
+# SEARCH PAGE
+departure_date = '//*[@id="ry-tooltip-6"]/div[2]/hp-app-controls-tooltips/fsw-controls-tooltips-container/fsw-controls-tooltips/fsw-flexible-datepicker-container/fsw-datepicker/ry-datepicker-desktop/div[1]/calendar[1]/calendar-body/div[4]/div[16]/div'
+city = '//*[@id="ry-tooltip-3"]/div[2]/hp-app-controls-tooltips/fsw-controls-tooltips-container/fsw-controls-tooltips/fsw-destination-container/fsw-airports/div/fsw-airports-list/div[2]/div[1]/fsw-airport-item[4]'
+return_date = '//*[@id="ry-tooltip-7"]/div[2]/hp-app-controls-tooltips/fsw-controls-tooltips-container/fsw-controls-tooltips/fsw-flexible-datepicker-container/fsw-datepicker/ry-datepicker-desktop/div[1]/calendar[2]/calendar-body/div[2]/div[10]/div'
+num_passengers = '/html/body/hp-app-root/hp-home-container/hp-home/hp-search-widget-container/hp-search-widget/div/hp-flight-search-widget-container/fsw-flight-search-widget-container/fsw-flight-search-widget/div/fsw-flight-search-widget-controls-container/fsw-flight-search-widget-controls/div[2]/fsw-input-button/div/div[2]'
+search_button = '/html/body/hp-app-root/hp-home-container/hp-home/hp-search-widget-container/hp-search-widget/div/hp-flight-search-widget-container/fsw-flight-search-widget-container/fsw-flight-search-widget/div/div/div/button'
+
+# FLIGHT DETAILS PAGE
+details = '/html/body/app-root/flights-root/div/div/flights-trip-details-container/flights-trip-details/div/div[2]'
+departure_flight = '/html/body/app-root/flights-root/div/div/div/div/flights-lazy-content/flights-summary-container/flights-summary/div/div[1]/journey-container/journey/flight-list/div/flight-card/div/div/div[3]/button'
+return_flight = '/html/body/app-root/flights-root/div/div/div/div/flights-lazy-content/flights-summary-container/flights-summary/div/div[2]/journey-container/journey/flight-list/div/flight-card/div/div/div[3]/button'
+regular_option = '/html/body/app-root/flights-root/div/div/div/div/flights-lazy-content/fare-selector-container/fare-selector/div/fare-table-container/fare-table/div[2]/ry-spinner/div[2]/div/div/fare-card/div/div/button'
+login_to_myryanair_section = '/html/body/app-root/flights-root/div/div/div/div/flights-lazy-content/flights-passengers/pax-app-container/pax-app/ry-spinner/ry-login-touchpoint-container/ry-login-touchpoint/div'
+passengers_section = '/html/body/app-root/flights-root/div/div/div/div/flights-lazy-content/flights-passengers/pax-app-container/pax-app/ry-spinner/div/div'
+login_later = '/html/body/app-root/flights-root/div/div/div/div/flights-lazy-content/flights-passengers/pax-app-container/pax-app/ry-spinner/ry-login-touchpoint-container/ry-login-touchpoint/div/button/div'
